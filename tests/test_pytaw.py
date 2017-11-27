0965ddb2c1db9fbb188d6e12d350f5289807b49c
6000hulls <6000hulls@gmail.com>
2017-11-27 15:23:07 +0000

    add some more tests

--- tests/test_pytaw.py
+++ tests/test_pytaw.py
@@ -1,12 +1,36 @@
-from pytaw import YouTube
 import pytest
+
+from pytaw import YouTube
 
 
 @pytest.fixture
 def youtube():
+    """A YouTube instance initialised with a developer key loaded from config.ini"""
     return YouTube()
 
 
-def test_video_title(youtube):
-    video = youtube.video(id_='jNQXAC9IVRw')
-    assert video.title == 'Me at the zoo'
+@pytest.fixture
+def video(youtube):
+    """A Video instance for the classic video 'Me at the zoo'"""
+    return youtube.video(id_='jNQXAC9IVRw')
+
+
+@pytest.fixture
+def channel(youtube):
+    """A Channel instance for the 'YouTube Help' channel"""
+    return youtube.channel(id_='UCMDQxm7cUx3yXkfeHa5zJIQ')
+
+
+class TestVideo(object):
+
+    def test_video_title(self, video):
+        assert video.title == "Me at the zoo"
+
+    def test_video_published_at(self, video):
+        assert video.published_at.isoformat() == '2005-04-24T03:31:52+00:00'
+
+
+class TestChannel(object):
+
+    def test_channel_title(self, channel):
+        assert channel.title == "YouTube Help"
